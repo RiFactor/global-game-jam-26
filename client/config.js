4ff935aef8726ebb5246c93904d5e41862527c62
fjebaker <fergus@cosroe.com>
2026-01-31 21:57:53 +0000

    feat: map rendering working

--- client/config.js
+++ client/config.js
@@ -1,9 +1,11 @@
 const SLOW = 0.1;
-const MEDIUM = 0.26;
+const MEDIUM = 0.36;
 const FAST = 0.24;
+
+const TILE_SIZE = 128;
 
 const DRAW_COLLISION = true;
 
 const URI = "ws://127.0.0.1:8000/";
 
-export { SLOW, MEDIUM, FAST, URI, DRAW_COLLISION };
+export { SLOW, MEDIUM, FAST, URI, DRAW_COLLISION, TILE_SIZE };
