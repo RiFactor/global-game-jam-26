54ed1ca0c9549aa0ed17e6518978d2acf902b428
fjebaker <fergus@cosroe.com>
2026-01-31 16:34:35 +0000

    feat(server): add a player store map

--- server/fileserver.js
+++ server/fileserver.js
@@ -30,7 +30,6 @@ async function requestHandler(req, res) {
     if (req.url == "/ws") return; // it's already been upgraded
 
     const file_path = path.join(STATIC_PATH, lookupPath(req.url));
-    console.log(file_path);
     const file_exists = await fs.promises.access(file_path).then(...toBool);
 
     var status_code = 200;
@@ -46,33 +45,58 @@ async function requestHandler(req, res) {
         res.end("Not found\n");
     }
 
-    console.log(`${req.method} ${req.url} ${status_code}`);
+    // console.log(`${req.method} ${req.url} ${status_code}`);
+}
+
+class PlayerState {
+    constructor(socket) {
+        this.socket = socket;
+        this.x = 0;
+        this.y = 0;
+        this.orientation = 0;
+        this.draw_state = 0;
+        this.mask = 0;
+    }
+
+    // Update the player state from an incoming message.
+    handleMessage(data) {
+        const msg = JSON.parse(data).content;
+        this.x = msg.x;
+        this.y = msg.y;
+        this.orientation = msg.orientation;
+        this.draw_state = msg.draw_state;
+        this.mask = msg.mask;
+    }
 }
 
 class ServerState {
     constructor(server, websocket_server) {
-        this.players = new Array();
+        this.players = new Map();
         this.server = server;
         this.websocket_server = websocket_server;
     }
 
-    onClientConnection(socket) {
-        console.log("Websocket connected");
+    onClientConnection(socket, req) {
+        const ip = req.socket.remoteAddress;
+        const port = req.socket._peername.port;
+
+        const player_id = `${ip}:${port}`;
+        const player = new PlayerState(socket);
+        this.players.set(player_id, player);
+
+        console.log(`Websocket connected from ${ip}:${port}`);
 
         socket.on("error", console.error);
+        socket.on("message", (data) => player.handleMessage(data));
 
-        socket.on("message", function message(data) {
-            console.log("received: %s", data);
-            const data_parsed = JSON.parse(data);
-
-            socket.send(JSON.stringify(data_parsed));
-        });
-
-        socket.send("Welcome, traveler.");
+        // Need to let the player known that their ID is
+        socket.send(JSON.stringify({ player_id: player_id }));
     }
 
     bind(port) {
-        this.websocket_server.on("connection", this.onClientConnection);
+        this.websocket_server.on("connection", (socket, req) =>
+            this.onClientConnection(socket, req),
+        );
         this.server.listen(port);
         console.log(`Server running at http://127.0.0.1:${PORT}/`);
     }
